e7e0aebfe3bf192d14950dbc74131cd1255bec25
Ágata Ordano <aordano@protonmail.com>
2022-09-09 16:17:09 -0300

    refactor(util): remove midi management functions

--- src/util.rs
+++ src/util.rs
@@ -2,10 +2,8 @@ use super::types;
 use colored::*;
 use config::{Config, ConfigError};
 use home::home_dir;
-use midir::{Ignore, MidiInput, MidiInputConnection};
-use std::error::Error;
 use std::path::PathBuf;
-use std::{env, fs, io, process};
+use std::process;
 
 pub fn read_user_config(path: Option<&String>) -> Result<types::ConfigFile, ConfigError> {
     let mut fullpath = PathBuf::new();
@@ -100,57 +98,4 @@ pub fn stdout(selector: &str, message: &str) {
             );
         }
     }
-}
-
-pub fn open_midi(
-    path: Option<&String>,
-    device: String,
-) -> Result<MidiInputConnection<()>, Box<dyn Error>> {
-    let mut midi_in = MidiInput::new("midir reading input")?;
-    midi_in.ignore(Ignore::None);
-
-    // Get an input port (read from config file)
-    let in_ports = midi_in.ports();
-    let in_port = match in_ports.len() {
-        0 => return Err("No input port found".into()),
-        _ => {
-            let mut selected_port = 0;
-
-            for (i, p) in in_ports.iter().enumerate() {
-                if midi_in
-                    .port_name(p)
-                    .unwrap()
-                    .as_str()
-                    .to_lowercase()
-                    .contains(&device.to_lowercase())
-                {
-                    selected_port = i;
-                }
-            }
-            in_ports.get(selected_port).ok_or("Device not found")?
-        }
-    };
-
-    println!("[Info] Opening connection");
-    let in_port_name = midi_in.port_name(in_port)?;
-
-    // _conn_in needs to be a named parameter, because it needs to be kept alive until the end of the scope
-    let conn_in = midi_in.connect(
-        in_port,
-        "midir-read-input",
-        move |_stamp, message, _| {
-            println!("[Debug] key: {}, value: {}", message[1], message[2]);
-        },
-        (),
-    )?;
-
-    return Ok(conn_in);
-
-    // println!(
-    //     "[Info] Connection open, reading input from '{}'",
-    //     in_port_name
-    // );
-
-    // println!("\n[Debug] Config: \n {:?}\n", config);
-    // loop {}
 }
