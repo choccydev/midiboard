d36e5202616f6bb998eab1aff4c61c311753710e
Ágata Ordano <aordano@protonmail.com>
2022-09-09 14:11:17 -0300

    fix(config): fix schema and validation to make less cumbersome to process the config file

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,5 @@
 use rust_embed::RustEmbed;
+use serde::Deserialize;
 use std::collections::HashMap;
 
 #[derive(RustEmbed, Debug)]
@@ -7,7 +8,7 @@ pub struct Asset;
 
 #[derive(Debug, Deserialize)]
 pub struct ConfigFile {
-    pub schema: String,
+    //    pub schema: String,
     pub config: Vec<Config>,
 }
 
@@ -20,21 +21,8 @@ pub struct Config {
 #[derive(Debug, Deserialize)]
 pub struct Input {
     pub key: u8,
-    pub kind: InputType,
+    //    pub kind: InputType,
     pub command: Command,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct EncoderCommand {
-    pub increase: String,
-    pub decrease: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct SwitchCommand {
-    pub on: String,
-    pub off: String,
-    pub initial_state: InitialSwitchState,
 }
 
 #[derive(Debug, Deserialize)]
@@ -44,9 +32,17 @@ pub enum InitialSwitchState {
 }
 
 #[derive(Debug, Deserialize)]
+#[serde(tag = "kind")]
 pub enum Command {
-    EncoderCommand,
-    SwitchCommand,
+    Encoder {
+        increase: String,
+        decrease: String,
+    },
+    Switch {
+        on: String,
+        off: String,
+        initial_state: InitialSwitchState,
+    },
 }
 
 #[derive(Debug, Deserialize)]
