fc57b1245e948d6a19eae015b0b9bb5f0954c053
Ágata Ordano <aordano@protonmail.com>
2022-09-09 16:17:49 -0300

    feat(run file): add boilerplate and base handling

--- src/run.rs
+++ src/run.rs
@@ -1,17 +1,77 @@
+use super::types;
 use super::util;
-use std::error::Error;
+use anyhow::Error;
+use midir::{Ignore, MidiInput, MidiInputConnection};
+use std::thread;
 
-pub fn run(cli: &clap::ArgMatches) -> Result<(), Box<dyn Error>> {
-    // TODO do stuff
+pub fn run(cli: &clap::ArgMatches) -> Result<(), Error> {
+    let path = cli.get_one::<String>("path");
 
-    match cli.subcommand() {
-        Some(("", sub_m)) => {
-            util::stdout("warning", "Please provide a subcommand. You can call this tool without arguments or with the --help flag for more information.")
-        }
-        _ => panic!(
-            "I before E, except when your foreign neighbor Keith received eight counterfeit beige sleights from feisty caffeinated weightlifters. Weird."
-        ),
+    let config_data = util::read_user_config(path)?;
+
+    for config in config_data.config {
+        thread::spawn(|| handle_device(config.device));
     }
-    // HACK add error handling
     Ok(())
+}
+
+pub fn open_midi(device: String) -> Result<MidiInputConnection<()>, Error> {
+    let mut midi_in = MidiInput::new("midir reading input")?;
+    midi_in.ignore(Ignore::None);
+
+    // Get an input port (read from config file)
+    let in_ports = midi_in.ports();
+    let in_port = match in_ports.len() {
+        0 => return Err(Error::msg("No devices found.")),
+        _ => {
+            let mut selected_port = 0;
+
+            for (i, p) in in_ports.iter().enumerate() {
+                if midi_in
+                    .port_name(p)
+                    .unwrap()
+                    .as_str()
+                    .to_lowercase()
+                    .contains(&device.to_lowercase())
+                {
+                    selected_port = i;
+                }
+            }
+            in_ports
+                .get(selected_port)
+                .ok_or(Error::msg("Device not found."))?
+        }
+    };
+
+    println!("[Info] Opening connection");
+    let conn_in = midi_in
+        .connect(
+            in_port,
+            "midir-read-input",
+            move |_stamp, message, _| {
+                // TODO add event sending here
+                //println!("[Debug] key: {}, value: {}", message[1], message[2]);
+            },
+            (),
+        )
+        .expect("Connection error."); // HACK bubble Err
+
+    return Ok(conn_in);
+}
+
+pub fn handle_device(device: String) -> Result<(), Error> {
+    match open_midi(device) {
+        Ok(connection) => {
+            todo!();
+            // TODO implement events and shit using the midi connection
+            // TODO find a way to manage errors
+            // TODO collect all keys and assign each to an event
+            // TODO add event listener for Switch event
+            // TODO add event listener for Encoder event
+            // TODO add debounce for encoders and switches
+            // TODO add command execution
+            // TODO add state handling to store the data to manage the Switch and Encoder events
+        }
+        Err(error) => return Err(error),
+    }
 }
