80185224e32aefdd7c24b1f73c6f2b1658062177
Ágata Ordano <aordano@protonmail.com>
2022-09-13 01:48:46 -0300

    feat(run): add base skeleton to handle key events

--- src/run.rs
+++ src/run.rs
@@ -1,8 +1,10 @@
 use super::types;
 use super::util;
 use anyhow::Error;
-use midir::{Ignore, MidiInput, MidiInputConnection};
+use midir::{Ignore, MidiInput};
+use std::collections::HashMap;
 use std::thread;
+use std::time::{Duration, Instant};
 
 pub fn run(cli: &clap::ArgMatches) -> Result<(), Error> {
     let path = cli.get_one::<String>("path");
@@ -10,12 +12,12 @@ pub fn run(cli: &clap::ArgMatches) -> Result<(), Error> {
     let config_data = util::read_user_config(path)?;
 
     for config in config_data.config {
-        thread::spawn(|| handle_device(config.device));
+        thread::spawn(move || handle_device(config.device.clone(), config));
     }
     Ok(())
 }
 
-pub fn open_midi(device: String) -> Result<MidiInputConnection<()>, Error> {
+pub fn handle_device(device: String, config: types::Config) -> Result<(), Error> {
     let mut midi_in = MidiInput::new("midir reading input")?;
     midi_in.ignore(Ignore::None);
 
@@ -43,35 +45,114 @@ pub fn open_midi(device: String) -> Result<MidiInputConnection<()>, Error> {
         }
     };
 
-    println!("[Info] Opening connection");
-    let conn_in = midi_in
+    let controls = get_controls(config.clone());
+
+    let mut states: HashMap<u8, Option<types::KeyState>> = HashMap::new();
+
+    for control in controls.clone() {
+        states.insert(control.0, None);
+    }
+
+    util::stdout("info", "Opening connection...");
+
+    let _conn_in = midi_in
         .connect(
             in_port,
-            "midir-read-input",
+            &device,
             move |_stamp, message, _| {
-                // TODO add event sending here
-                //println!("[Debug] key: {}, value: {}", message[1], message[2]);
+                let key = message[1];
+                let value = message[2];
+
+                match states.get(&key) {
+                    Some(state) => {
+                        util::stdout(
+                            "",
+                            format!("Control {} detected.", &controls.get(&key).unwrap()).as_str(),
+                        );
+                        let key_combo_state =
+                            on_key_event(key, state.clone(), &config, &controls, value);
+
+                        match key_combo_state.0 {
+                            true => {
+                                match call_command(key.clone()) {
+                                    Ok(command) => util::stdout(
+                                        "info",
+                                        format!("Executed command {}", command).as_str(),
+                                    ),
+                                    Err(error) => util::stdout("error", &error.to_string()),
+                                };
+                                states.remove(&key);
+                                states.insert(key, None);
+                            }
+                            false => {}
+                        }
+                    }
+                    None => {}
+                }
             },
             (),
         )
         .expect("Connection error."); // HACK bubble Err
 
-    return Ok(conn_in);
+    util::stdout("info", "Connection closed.");
+
+    Ok(())
 }
 
-pub fn handle_device(device: String) -> Result<(), Error> {
-    match open_midi(device) {
-        Ok(connection) => {
-            todo!();
-            // TODO implement events and shit using the midi connection
-            // TODO find a way to manage errors
-            // TODO collect all keys and assign each to an event
-            // TODO add event listener for Switch event
-            // TODO add event listener for Encoder event
-            // TODO add debounce for encoders and switches
-            // TODO add command execution
-            // TODO add state handling to store the data to manage the Switch and Encoder events
-        }
-        Err(error) => return Err(error),
+pub fn call_command(key: u8) -> Result<String, Error> {
+    todo!();
+}
+
+pub fn get_controls(config: types::Config) -> types::ControlList {
+    let mut list = HashMap::new();
+
+    for control in config.controls.clone() {
+        list.insert(control.1.key, control.0);
     }
+    list
+}
+
+pub fn on_key_event(
+    key: u8,
+    state: Option<types::KeyState>,
+    config: &types::Config,
+    controls: &types::ControlList,
+    value: u8,
+) -> (bool, Option<types::KeyState>) {
+    match controls.get(&key) {
+        None => return (false, None),
+        Some(somekey) => match state {
+            None => {
+                let new_state = Some(types::KeyState {
+                    control: somekey.clone(),
+                    time_threshold: Duration::from_millis(
+                        get_threshold_from_key(key, config, controls).detection,
+                    ),
+                    activation_threshold: Duration::from_millis(
+                        get_threshold_from_key(key, config, controls).activation,
+                    ),
+                    detections: Vec::new(),
+                    start: Instant::now(),
+                });
+                return (false, new_state);
+            }
+            Some(state) => {
+                // TODO do stuff with the state to detect new values and check if there is a new activation
+
+                // TODO debounce
+
+                // TODO add branches for encoder and for switch
+                todo!()
+            }
+        },
+    }
+}
+
+pub fn get_threshold_from_key(
+    key: u8,
+    config: &types::Config,
+    controls: &types::ControlList,
+) -> types::TimeThreshold {
+    todo!();
+    // TODO cycle and collect config to determine if the key is encoder or switch and return the correct TimeThreshold
 }
--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,9 @@
 use rust_embed::RustEmbed;
 use serde::Deserialize;
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    time::{Duration, Instant},
+};
 
 #[derive(RustEmbed, Debug)]
 #[folder = "schema/"]
@@ -11,38 +14,38 @@ pub struct ConfigFile {
     pub config: Vec<Config>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct Config {
     pub device: String,
     pub controls: HashMap<String, Input>,
     pub thresholds: Thresholds,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct Thresholds {
-    encoder: TimeThreshold,
-    switch: TimeThreshold,
+    pub encoder: TimeThreshold,
+    pub switch: TimeThreshold,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct TimeThreshold {
-    activation: u32,
-    detection: u32,
+    pub activation: u64,
+    pub detection: u64,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct Input {
     pub key: u8,
     pub command: Command,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub enum InitialSwitchState {
     ON,
     OFF,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 #[serde(tag = "kind")]
 pub enum Command {
     Encoder {
@@ -55,3 +58,18 @@ pub enum Command {
         initial_state: InitialSwitchState,
     },
 }
+
+#[derive(Debug, Clone)]
+pub struct KeyState {
+    // Target control of the detected key
+    pub control: String,
+    // Minimum time threshold in ms from last key detection to add here
+    pub time_threshold: Duration,
+    // Elapsed time since first activation to consider activation finished
+    pub activation_threshold: Duration,
+    // value recorded at each detection
+    pub detections: Vec<u8>,
+    pub start: Instant,
+}
+
+pub type ControlList = HashMap<u8, String>; // HashMap<key code, control name>
